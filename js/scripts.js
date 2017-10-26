e1cbbaa655ffe9aeee20df61856e68d549a77477
Amy Churchwell and Griffin P Longtin <griffin@>
2017-10-26 11:11:35 -0700

    create function to determine values for tens

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,6 @@ var romeC = "C";
 var romeD = "D";
 var romeM = "M";
 
-function runInput(number){
-  if (number.length = 4){
-    var one = number[3];
-    var ten = number[2];
-    var h = number[1];
-    var th = number[0]
-
-  } else if (number.length = 3) {
-    var one = number[2];
-    var ten = number[1];
-    var h = number[0];
-  } else if (number.length = 2) {
-    var one = number[1];
-    var ten = number[0];
-  } else {
-    var one = number;
-  }
-};
-
 function ones(number) {
   if (number[0] <= 3) {
     return romeI.repeat(number[0]);
@@ -41,17 +22,11 @@ function ones(number) {
 }
 
 function tens(number) {
-  if (number[0] <= 3) {
-    return romeI.repeat(number[0]);
-  }else if (number[0] <= 4) {
-    return romeI + romeV;
 
-  }else if (number[0] <= 5){
-    return romeV;
-
-  }else if (number[0] <= 9){
-    var num = (number[0] - 5)
-    return romeV + romeI.repeat(num);
+  if ((number[0] >= 1) && (number[0] <= 4)) {
+    return romeX.repeat(number[0]) + ones(number[1]);
+  } else if (number[0] >= 5){
+    return romeL + ones(number[1]);
   }
 }
 
@@ -61,6 +36,6 @@ $(function(){
     event.preventDefault();
     var input = $('#input').val();
 
-    $("#output").text(ones(input));
+    $("#output").text(tens(input));
   });
 });
