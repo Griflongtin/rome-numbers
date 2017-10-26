67ec4d0af3186638d99cda36ada875537c0b091b
Amy Churchwell and Griffin P Longtin <griffin@>
2017-10-26 10:51:05 -0700

    create function to determine values of indices

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,57 @@
-var number
+var romeI = "I";
+var romeV = "V";
+var romeX = "X";
+var romeL = "L";
+var romeC = "C";
+var romeD = "D";
+var romeM = "M";
 
-// function thousands(number) {
-//   debugger
-//   if () {
-//
-//     }
-//   } else if (number[3] === 4) {
-//
-//   } else {
-//
-//   }
-// }
-//
-// function hundreds(number) {
-//
-// }
-//
-// function tens(number) {
-//
-// }
+function runInput(number){
+  if (number.length = 4){
+    var one = number[3];
+    var ten = number[2];
+    var h = number[1];
+    var th = number[0]
 
+  } else if (number.length = 3) {
+    var one = number[2];
+    var ten = number[1];
+    var h = number[0];
+  } else if (number.length = 2) {
+    var one = number[1];
+    var ten = number[0];
+  } else {
+    var one = number;
+  }
+};
 
-//CURRENTLY IF # IS 1 DIGIT//
 function ones(number) {
-  var numberRome = "I";
-
   if (number[0] <= 3) {
-    number[0]
-    return numberRome.repeat(number[0]);
+    return romeI.repeat(number[0]);
   }else if (number[0] <= 4) {
-  }else {
-    console.log("no");
+    return romeI + romeV;
+
+  }else if (number[0] <= 5){
+    return romeV;
+
+  }else if (number[0] <= 9){
+    var num = (number[0] - 5)
+    return romeV + romeI.repeat(num);
+  }
+}
+
+function tens(number) {
+  if (number[0] <= 3) {
+    return romeI.repeat(number[0]);
+  }else if (number[0] <= 4) {
+    return romeI + romeV;
+
+  }else if (number[0] <= 5){
+    return romeV;
+
+  }else if (number[0] <= 9){
+    var num = (number[0] - 5)
+    return romeV + romeI.repeat(num);
   }
 }
 
